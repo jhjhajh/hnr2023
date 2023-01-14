b4cb09fbb2f3f7eede614918ca75a96deff1370c
Jia Hui <angjiahui90@gmail.com>
2023-01-14 17:29:24 +0800

    version that works and added .env to gitignore

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import logging
+from os import environ as env
+from dotenv import load_dotenv
 
 import telebot
 import openai
@@ -6,58 +8,27 @@ import openai
 logger = telebot.logger
 telebot.logger.setLevel(logging.DEBUG)
 
-bot = telebot.TeleBot("5953540475:AAGLYArxfKAURJlkhYBEhTiQxCszToKRA4g")
-openai.api_key = "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"
+load_dotenv()
+bot = telebot.TeleBot(token=env["BOT_API_KEY"])
+openai.api_key=env["OPENAI_API_KEY"]
 
 @bot.message_handler(func=lambda message: True)
-
-# bot.send_message(message.chat.id, text = "Welcome!")
-
 def get_response(message):
-
-    prompt = message.text
     
-    response = openai.Completion.create(engine="text-davinci-002", prompt=prompt)
+    prompt_message = "explain fun " + message.text + " in a fun and weird but short way without mentioning the word" + message.text + " Do not explicitly mention words related to " + message.text
 
-    print(response["choices"][0]["text"])
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt='"""\n{}\n"""'.format(prompt_message),
+        temperature=0,
+        max_tokens=1200,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=['"""'])
 
-    bot.send_message(message.chat.id, text = response["choices"][0]["text"])
-    # bot.send_message(message.chat.id, text = response["choices"][0]["text"])
+    bot.send_message(message.chat.id,
+    f'```python\n{response["choices"][0]["text"]}\n```',
+    parse_mode="Markdown")
 
 bot.infinity_polling()
-
-# # from teleBot import CommandHandler, Updater
-# # from logging import basicConfig, getLogger, INFO
-
-# # basicConfig(level=INFO)
-# # log = getLogger()
-
-# # def start(update, context):
-# #     update.message.reply_text(
-# #         "start this bot",
-# #         parse_mode="markdown")
-
-# # def help(update, context):
-# #     update.message.reply_text(
-# #         "help for this bot",
-# #         parse_mode="markdown")
-
-# # def main():
-# #     updater = Updater(token=BOT_TOKEN, use_context=True)
-# #     dispatcher = updater.dispatcher
-
-# #     start_handler = CommandHandler("start", start)
-# #     help_handler = CommandHandler("help", help)
-
-# #     dispatcher.add_handler(start_handler)
-# #     dispatcher.add_handler(help_handler)
-# #     updater.start_polling()
-
-# # if __name__ == '__main__':
-# #     main()
-
-# import os
-import openai
-# openai.organization = "org-mKz403VBDaLqeEP7mJB80GEa"
-openai.api_key = "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"
-openai.Model.list()
